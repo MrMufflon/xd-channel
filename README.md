10dc4df5651d1b9ffa5a6e999f0a7ede969ed683
Felix Bache <felix.bache@gmail.com>
2013-07-24 22:04:27 +0200

    first committ

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 xd-channel
 ==========
 
-communication between cross domain iframes in javascript
+Communication between cross domain iframes in javascript based  on window.postMessage(). If not supported by browser a fallback through hashtag communication is used.
+
+Using [JSON-js](https://github.com/douglascrockford/JSON-js) for cross-browser JSON parsing.
+
+Supported data types:
+- String
+- JSON object
+
+### Usage
+```javascript
+require(["xd-channel"], function(channel) {
+var c = channel.createChannel({
+	//target, optional, default window.parent
+	target:document.getElementById("iframe"),
+	//allowed domain for communication in this channel
+	domain:"http://localhost:8081",
+	//complete communication target url (used for #-fallback)
+	targetUrl:"/xd-channel/test/child.html",
+	//callback to invoke on message
+	receiveCallback:function(evt){
+		//evt.data contains the received data
+	}
+});
+c.post({value:"some message", something:["a","b"]});
+});
+```
+	
---
+++ src/xd-channel.js
@@ -0,0 +1,100 @@
+define(["json2"], function() {
+
+	return {
+
+		createChannel: function(opts) {
+			var target = opts.target.postMessage ? opts.target : (opts.target.contentWindow || opts.target);
+			var channel = {
+
+				target: target,
+				allowedDomain: opts.domain,
+				receiveCallback: opts.receiveCallback,
+				cbScope: opts.recieveCallbackScope || this,
+				targetUrl: opts.targetUrl,
+				pollInterval: opts.pollInterval || 100,
+				_hasPostMessage: false,
+				_lastHash: "",
+
+				init: function() {
+					this._cb = this.receive.bind(this);
+					this.receiveCallback.bind(this.cbScope);
+					if (window.postMessage) {
+						this._hasPostMessage = true;
+					}
+					if (this._hasPostMessage) {
+						if (window["addEventListener"]) {
+							window["addEventListener"]("message", this._cb, false);
+						} else {
+							window["attachEvent"]("onmessage", this._cb);
+						}
+					} else {
+						this._pollTimer = setInterval(this._pollMessage.bind(this), this.pollInterval);
+					}
+
+				},
+
+				_pollMessage: function() {
+					var hash = document.location.hash;
+					if (this._lastHash !== hash) {
+						var url = document.location.href,
+							cachebuster = /^#?\d+&/;
+						url = url.replace(/#.*$/, "");
+						if (!hash || !hash.length > 0) {
+							return;
+						}
+						this._lastHash = hash;
+						document.location = url + "#";
+						var msg = decodeURIComponent(hash.replace(cachebuster, ""));
+						var data = msg;
+						try {
+							data = JSON.parse(msg);
+						} catch (e) {}
+						this.receive({
+							data: data,
+							origin: this.allowedDomain
+						});
+					}
+				},
+
+				destroy: function() {
+					if (window["removeEventListener"]) {
+						window["removeEventListener"]("message", this._cb, false);
+					} else {
+						window["detachEvent"]("onmessage", this._cb);
+					}
+					if (this._pollTimer) {
+						clearInterval(this._pollTimer);
+					}
+				},
+
+				post: function(data) {
+					console.debug("post message to domain " + this.allowedDomain, data);
+					if (this._hasPostMessage) {
+						this.target.postMessage(data, this.allowedDomain || "*");
+					} else {
+						var targetUrl = this.allowedDomain + this.targetUrl;
+						if (data instanceof Object) {
+							data = JSON.stringify(data);
+						}
+						this.target.location = targetUrl.replace(/#.*$/, '') + "#" + (new Date().getTime() + "&" + encodeURIComponent(data));
+					}
+				},
+
+				receive: function(evt) {
+					console.debug("received message from " + evt.origin, evt.data);
+					//todo check domains
+					if (evt.origin && evt.origin === this.allowedDomain) {
+						this.receiveCallback(evt);
+					}
+				}
+
+			};
+
+			channel.init();
+			return channel;
+
+		}
+
+	};
+
+});
